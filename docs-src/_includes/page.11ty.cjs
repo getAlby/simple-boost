be1e182c7a8be7a09013cb5e712a0c485e4538da
Dunsin <ayodejiabisuwa23@gmail.com>
2025-08-25 17:28:05 +0100

    fix: lose html syntax to allow vite bundle without error

--- docs-src/_includes/page.11ty.cjs
+++ docs-src/_includes/page.11ty.cjs
@@ -13,7 +13,7 @@ module.exports = function (data) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${title}</title>
-    <meta name="alby:name" content="Alby - simple-boost"></meta>
+    <meta name="alby:name" content="Alby - simple-boost">
     <link rel="stylesheet" href="${relative(page.url, '/docs.css')}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600|Roboto+Mono">
     <link href="${relative(page.url, '/prism-okaidia.css')}" rel="stylesheet" />
